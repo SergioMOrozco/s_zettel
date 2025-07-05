e102b97d3f1d6318dbed75bea1348e317fdab56b
Sergio Orozco <sorozco@theaiinstitute.com>
2025-07-05 19:53:27 -0400

    added way to edit files, and adding way to link them together

--- s_zettel/cli.py
+++ s_zettel/cli.py
@@ -1,7 +1,37 @@
 import click
 from datetime import datetime
 from pathlib import Path
+#from ops import new_file, new_metdata
 import subprocess
+
+def new_file(timestamp, metadata= False):
+
+    if metadata:
+
+        # create metadata dir if it doesn't exist
+        metadata_dir = Path("./metadata")
+        metadata_dir.mkdir(parents=True, exist_ok=True)
+
+        filename = Path(f"./metadata/{timestamp}.json")
+    else:
+        filename = Path(f"{timestamp}.md")
+
+    # Create the file if it doesn't exist
+    if not filename.exists():
+        filename.write_text(f"# Unique Title\n")
+        click.echo(f"Created {filename}")
+    else:
+        click.echo(f"File {filename} already exists.")
+
+    return filename
+
+def new_metadata(timestamp):
+
+    filename = new_file(timestamp, metadata=True)
+
+    filename.write_text("{ }")
+
+    return filename
 
 @click.group()
 def ztl():
@@ -14,25 +44,43 @@ def new():
     # Format the filename: "2025-07-05.md"
     timestamp = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
 
-    filename = Path(f"{timestamp}.md")
+    filename = new_file(timestamp)
+    filename_metadata = new_metadata(timestamp)
 
-    # Create the file if it doesn't exist
-    if not filename.exists():
-        filename.write_text(f"# Notes for {timestamp}\n")
-        click.echo(f"Created {filename}")
-    else:
-        click.echo(f"File {filename} already exists.")
 
     # Open the file in Vim
     subprocess.run(["vim", str(filename)])
 
 @ztl.command()
-def edit():
-    click.echo("Called: ztl edit")
+@click.option('--file', '-f', type=click.Path(exists=True), required=True, help="Markdown file to edit")
+def edit(file):
+    """Edit a markdown file."""
+    filepath = Path(file)
+
+    # Open the file in Vim
+    subprocess.run(["vim", str(filepath)])
+
 
 @ztl.command()
 def list():
-    click.echo("Called: ztl list")
+
+    md_files = Path("./").glob("*.md")
+
+    for md_file in md_files:
+        with open(md_file, "r", encoding="utf-8") as f:
+            for line in f:
+                line = line.strip()
+                if line.startswith("#"):
+                    print(f"{md_file.name} \t ==> \t {line}")
+                    break
+            else:
+                print(f"{md_file.name} \t ==> \t (no title found)")
+
+@ztl.command()
+@click.option('--source', '-s', type=click.Path(exists=True), required=True, help="Source file of relationship")
+@click.option('--target', '-t', type=click.Path(exists=True), required=True, help="Target file of relationship")
+def link():
+    click.echo("Called: ztl link")
 
 if __name__ == "__main__":
     ztl()
